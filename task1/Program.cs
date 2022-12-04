cb53fc831e484b0ad026e94817d2e64dc82f387c
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-04 11:14:16 +0500

    Add new commit

---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Возвращает рандомное число из отрезка [left, right]
+int GetRandomNumber(int left, int right)
+{
+    Random rnd = new Random();
+    int number = rnd.Next(left,right + 1); //[left, right]
+    return number;
+}
+
+// Возвращает максимальную цифру числа
+int GetMaxDigitOfNumber(int number)
+{
+    int firstDigit = number / 10;
+    int secondDigit = number % 10;
+    if(firstDigit > secondDigit)
+        return firstDigit;
+    else
+        return secondDigit;
+}
+
+int number = GetRandomNumber(10, 99);
+
+Console.WriteLine($"Сгенерировать число: {number}");
+
+int maxDigit = GetMaxDigitOfNumber(number);
+
+Console.WriteLine($"Наибольшая цифрв числа: {maxDigit}");
---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int GetRandomNumber()
+{
+    return new Random().Next(100, 1000);
+}
+
+
+void ShowRandomNumber(int number)
+{
+    Console.WriteLine($"Случайной трёхзначное число: {number}");
+    int result = (((number / 100) * 10 ) + (number % 10));
+    Console.WriteLine(result);
+}
+
+ShowRandomNumber(GetRandomNumber());
